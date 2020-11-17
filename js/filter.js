1e7b5df2c28982a16ab1c02adf1ba345875ab13e
guilt-alt <s.a.s-99@yandex.com>
2020-11-18 05:08:32 +0600

    правки

--- js/filter.js
+++ js/filter.js
@@ -6,63 +6,49 @@ const housingPrice = mapFilters.querySelector(`#housing-price`);
 const housingRooms = mapFilters.querySelector(`#housing-rooms`);
 const housingGuests = mapFilters.querySelector(`#housing-guests`);
 const housingFeatures = mapFilters.querySelectorAll(`.map__checkbox`);
-const filters = [
-  housingType,
-  housingPrice,
-  housingRooms,
-  housingGuests
-];
+
+const Price = {
+  LOW: 10000,
+  HIGH: 50000
+};
+
+const filterByHouse = (type) => housingType.value === `any` || housingType.value === type;
+const filterByRooms = (rooms) => housingRooms.value === `any` || Number(housingRooms.value) === rooms;
+const filterByGuests = (guests) => housingGuests.value === `any` || Number(housingGuests.value) === guests;
+
+const filterByPrice = (price) => {
+  switch (housingPrice.value) {
+    case `low`:
+      return price < Price.LOW;
+    case `middle`:
+      return price >= Price.LOW && price <= Price.HIGH;
+    case `high`:
+      return price > Price.HIGH;
+    default:
+      return true;
+  }
+};
+
+const filterByFeatures = (feature) => {
+  for (let housingFeature of housingFeatures) {
+    if (housingFeature.checked && !feature.includes(housingFeature.value)) {
+      return false;
+    }
+  }
+  return true;
+};
 
 const updatePins = () => {
   let data = window.load.data;
   window.cards.close();
   window.pins.removePins();
 
-  const getFilteredData = (value) => {
-    data = data.filter((pin) => {
-      switch (value) {
-        case housingType.value:
-          return pin.offer.type === value;
-        case housingRooms.value:
-          return pin.offer.rooms === Number(value);
-        case housingGuests.value:
-          return pin.offer.guests === Number(value);
-        case `middle`:
-          return (pin.offer.price >= 10000) && (pin.offer.price <= 50000);
-        case `low`:
-          return pin.offer.price < 10000;
-        case `high`:
-          return pin.offer.price > 50000;
-        case `wifi`:
-          return pin.offer.features.includes(value);
-        case `dishwasher`:
-          return pin.offer.features.includes(value);
-        case `parking`:
-          return pin.offer.features.includes(value);
-        case `washer`:
-          return pin.offer.features.includes(value);
-        case `elevator`:
-          return pin.offer.features.includes(value);
-        case `conditioner`:
-          return pin.offer.features.includes(value);
-        default:
-          return false;
-      }
-    });
-  };
-
-  housingFeatures.forEach((housingFeature) => {
-    if (housingFeature.checked) {
-      const feature = housingFeature.value;
-      getFilteredData(feature);
-    }
-  });
-
-  filters.forEach((filter) => {
-    const selectValue = filter.value;
-    if (selectValue !== `any`) {
-      getFilteredData(selectValue);
-    }
+  data = data.filter((item) => {
+    return filterByHouse(item.offer.type) &&
+      filterByPrice(item.offer.price) &&
+      filterByRooms(item.offer.rooms) &&
+      filterByGuests(item.offer.guests) &&
+      filterByFeatures(item.offer.features);
   });
 
   window.pins.createPins(data);
